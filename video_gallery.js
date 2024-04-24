01f7144731ffa79716b1926471261dda0f16699c
yateen sharma <yatinsharma1007@gmail.com>
2024-04-24 12:40:14 +0530

    change will bw done

---
+++ video_gallery.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper('.swiper-container', {
+    direction: 'vertical',
+    slidesPerView: 1,
+    spaceBetween: 0,
+    effect: 'slide',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesOffsetBefore: 0,
+    slidesOffsetAfter: 0,
+    loop: true,
+    loopedSlides: 2,
+    // Remove autoplay
+});
